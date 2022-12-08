a89ebf89e4747bd47903c8f1f80e3e0f86668b5a
abrarzahed <abrarzahed6986@gmail.com>
2022-12-08 16:06:51 +0600

    completed counter app with redux cdn

--- module-1/state-counter/html_template/script.js
+++ module-1/state-counter/html_template/script.js
@@ -1,3 +1,4 @@
+/*
 const increment = document.getElementById("increment");
 const decrement = document.getElementById("decrement");
 const resultEL = document.getElementById("result");
@@ -11,4 +12,60 @@ increment.addEventListener("click", () => {
 decrement.addEventListener("click", () => {
   state--;
   resultEL.textContent = state;
+});
+*/
+
+/* 
+  COMMENT: With redux CDN
+*/
+
+// select dom elements
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+const resultEL = document.getElementById("result");
+
+// initial state
+const initialState = {
+  value: 0,
+};
+
+// create reducer function
+function counterReducer(state = initialState, action) {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      value: state.value + 1,
+    };
+  } else if (action.type === "decrement") {
+    return {
+      ...state,
+      value: state.value - 1,
+    };
+  } else {
+    return state;
+  }
+}
+
+// create store
+// eslint-disable-next-line no-undef
+const store = Redux.createStore(counterReducer);
+function render() {
+  const state = store.getState();
+  resultEL.textContent = String(state.value);
+}
+
+// update UI
+render();
+store.subscribe(render);
+
+// action dispatch
+increment.addEventListener("click", () => {
+  store.dispatch({
+    type: "increment",
+  });
+});
+decrement.addEventListener("click", () => {
+  store.dispatch({
+    type: "decrement",
+  });
 });
