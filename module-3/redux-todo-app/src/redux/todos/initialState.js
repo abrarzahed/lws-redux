ceb1b2b471fcb266cb5422b234b1df5d1a116184
abrarzahed <abrarzahed6986@gmail.com>
2022-12-20 15:25:15 +0600

    added redux setup and compose with devtools

---
+++ module-3/redux-todo-app/src/redux/todos/initialState.js
@@ -0,0 +1,13 @@
+export const initialState = [
+  {
+    id: 1,
+    title: "Learn react js",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Learn react js",
+    completed: false,
+    color: "red",
+  },
+];
