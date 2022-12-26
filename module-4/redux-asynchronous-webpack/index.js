15bb997e8deb1a15b099157b0b6185fad8d919b7
abrarzahed <abrarzahed6986@gmail.com>
2022-12-26 23:02:06 +0600

    initilaized module 4

---
+++ module-4/redux-asynchronous-webpack/index.js
@@ -0,0 +1,45 @@
+const { createStore, applyMiddleware } = require("redux");
+
+// initial state
+const initialState = {
+  todos: [],
+};
+
+// reducer
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "todo/added":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            title: action.payload,
+          },
+        ],
+      };
+
+    case "todo/loaded":
+      return {
+        ...state,
+        todos: [...state.todos, ...action.payload],
+      };
+
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(todoReducer, applyMiddleware());
+
+// subscribe to state changes
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// dispatch actions
+store.dispatch({
+  type: "todo/added",
+  payload: "Learn Redux",
+});
