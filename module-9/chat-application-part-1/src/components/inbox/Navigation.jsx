2336cdcf0792a756c79a75811f7eb03a9eec26a7
abrarzahed <abrarzahed6986@gmail.com>
2023-02-05 12:47:32 +0600

    implemented private route

--- module-9/chat-application-part-1/src/components/inbox/Navigation.jsx
+++ module-9/chat-application-part-1/src/components/inbox/Navigation.jsx
@@ -1,7 +1,14 @@
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import logoImage from "../../assets/images/lws-logo-dark.svg";
+import { userLoggedOut } from "../../features/auth/authSlice";
 
 export default function Navigation() {
+  const dispatch = useDispatch();
+  const logout = () => {
+    dispatch(userLoggedOut());
+    localStorage.clear();
+  };
   return (
     <nav className="border-general sticky top-0 z-40 border-b bg-violet-700 transition-colors">
       <div className="max-w-7xl mx-auto">
@@ -11,7 +18,9 @@ export default function Navigation() {
           </Link>
           <ul>
             <li className="text-white">
-              <Link to="/">Logout</Link>
+              <span onClick={logout} className="cursor-pointer">
+                Logout
+              </span>
             </li>
           </ul>
         </div>
