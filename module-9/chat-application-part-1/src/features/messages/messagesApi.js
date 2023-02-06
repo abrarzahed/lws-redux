fb4f6f8fd571e949a88dbcf3b1a5eb7def1a2963
abrarzahed <abrarzahed6986@gmail.com>
2023-02-06 18:55:20 +0600

    get messages

--- module-9/chat-application-part-1/src/features/messages/messagesApi.js
+++ module-9/chat-application-part-1/src/features/messages/messagesApi.js
@@ -5,5 +5,11 @@ export const messagesApi = apiSlice.injectEndpoints({
     /* 
       COMMENT: endpoints goes here
     */
+    getMessages: builder.query({
+      query: (id) =>
+        `/messages?conversationId=${id}&_sort=timestamp&_order=desc&_page=1&_limit==6`,
+    }),
   }),
 });
+
+export const { useGetMessagesQuery } = messagesApi;
